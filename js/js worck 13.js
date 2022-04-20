634950419364659b5f1fd1567adda28c223ecb91
Your Name <you@example.com>
2022-04-20 10:11:24 +0300

    1 commit

---
+++ js/js worck 13.js
@@ -0,0 +1,17 @@
+console.log('arr'+ " - object");
+console.log(4 + " - object");
+let incr = 10,
+    desc = 10;
+
+    ////incr++;
+    ///desc--;
+    console.log(++incr);
+    console.log(--desc);
+    console.log(5%2);
+    console.log(2*5==10);
+    console.log(2+2*2 !==8);
+    const isChecked = false,
+          isClose = false;
+
+console.log(isClose|| !isChecked);
+    

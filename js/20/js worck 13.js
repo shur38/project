9044361a193ded552d9a1148271a8b87c6339ad7
shur38 <shur3225@gmail.com>
2022-04-21 16:35:02 +0300

    lesson 20 new file

--- js/20/js worck 13.js
+++ js/20/js worck 13.js
@@ -1,6 +1,33 @@
-if (4==9){
-    console.log('ok!');
+// if (4==9){
+//     console.log('ok!');
 
+// }else{
+// console.log('error!');
+// }
+
+const nam = 50;
+if (nam<33){
+    console.log('error!');
+}else if (nam > 100){
+    console.log('много');
 }else{
-console.log('error!');
+    console.log('ok!')
+}
+(nam ===50) ? console.log('ok!') : console.log('Error');
+
+const nam =50;
+
+switch (nam){
+    case 49:
+        console.log('wrong');
+        break;
+        case 100:
+            console.log('wrong');
+            break;
+            case 50:
+            console.log('this true');
+            break;
+            default:
+                console.log('true ok!');
+                break;
 }

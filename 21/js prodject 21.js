1234098b993272bff9d5ca951201e8de621733f0
shur38 <shur3225@gmail.com>
2022-04-26 14:15:32 +0300

    циклы и метки

--- 21/js prodject 21.js
+++ 21/js prodject 21.js
@@ -1,32 +1,32 @@
-    // const hamburger = 5;
-    // const fries = 0;
-    // if (hamburger && fries){
-    //     console.log('я сыт!');
-    // }
-    // console.log((hamburger && fries));
-    // const hamburger = 3;
-    // const fries = 1;
-    // const cola = 1 ;
+    const hamburger = 5;
+    const fries = 0;
+    if (hamburger && fries){
+        console.log('я сыт!');
+    }
+    console.log((hamburger && fries));
+    const hamburger = 3;
+    const fries = 1;
+    const cola = 1 ;
 
-    // console.log(hamburger ===3 && cola && fries );
-    // console.log(1  &&  0);
-    // console.log(1  &&  5);
-    // console.log(null  &&  5);
-    // console.log(0  &&  "oll by write");
+    console.log(hamburger ===3 && cola && fries );
+    console.log(1  &&  0);
+    console.log(1  &&  5);
+    console.log(null  &&  5);
+    console.log(0  &&  "oll by write");
 
 
 
-    //  if (hamburger ===3 && cola && fries ){  
-    //     console.log('я сыт!');
+     if (hamburger ===3 && cola && fries ){  
+        console.log('я сыт!');
 
-    // }else{
-    //     console.log('мы уходим в другую столовую');
-    // }
+    }else{
+        console.log('мы уходим в другую столовую');
+    }
    const hamburger = 3 ;
    const fries = 3;
-   const cola =0;
+   const colas =0;
    const nagets = 2;
-   
+   console.log((hamburger === 2 && cola === 2 || fries === 3 && nagets));
    if (hamburger === 2 && cola === 2 || fries === 3 && nagets){
        console.log('oll by write ! ');
    }else{
@@ -36,3 +36,21 @@
 
    let johnRaport , alexRaport , samRaport , marijaRaport = 'done';
    console.log( johnRaport || alexRaport || samRaport || marijaRaport);
+
+   console.log(!0);
+
+console.log(NaN || 2 || undefined);
+
+console.log(NaN && 2 && undefined);
+
+console.log(1 && 2 && 3);
+
+console.log(!1 && 2 || !3);
+
+console.log(25 || null && !3);
+
+console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 || undefined || 5);
+
+console.log(5 === 5 && 3 > 1 || 5);
--- 21/while 22 .js
+++ 21/while 22 .js
@@ -1,4 +1,4 @@
-let num =50;
+// let num =50;
 // while(num<=55){ 
 //     console.log(num);
 //     num++;
@@ -8,7 +8,62 @@ let num =50;
 //     num++;
 // }
 // while(num<=55);
-for(let i =1; i < 8; i++){
-    console.log(num);
-    num++;
-}
+// for(let i =1; i <= 10; i++){
+//     if(i===6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+    
+// } 
+// let r = 0;
+// while (r++   < 5){
+//     console.log( r );
+    
+// } 
+
+// let i = 0;
+// while (i++ < 5) console.log( i );
+
+
+//     for (number =0;number < 100; number++){
+//      console.log(number);
+      
+
+
+        
+//       }
+// for (let i = 0; i < 10; i++) {
+
+//     // если true, пропустить оставшуюся часть тела цикла
+//     if (i % 2 !==1) continue;
+  
+//     console.log(i); // 1, затем 3, 5, 7, 9
+
+} //   }
+for (i = 0; i < 3; i++) {
+
+  for (j = 0; j < 3; j++) {
+    console.log(j, i);
+
+  }
+  let resuit = '';
+  const lenght = 7;
+  for (let i = 1; i < lenght; i++) {
+    for (let j = 0; j < i; j++) {
+      resuit += '*';
+    }
+    resuit += '\n'
+  }
+  console.log(resuit)
+  first: for (i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (j = 0; j < 3; j++) {
+      console.log(`Second level: ${j}`);
+      for (k = 0; k < 5; k++) {
+        if (k === 2) break first;// or continue
+        console.log(`Third level: ${k}`);
+
+      }
+    }
+  }
